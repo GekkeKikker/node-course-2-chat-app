91b76a60bcc4e347844be67a47424ede4593e690
GekkeKikker <maarten.alles@pandora.be>
2018-08-24 17:28:36 +0200

    add newMessage and CreateMessage events

--- server/server.js
+++ server/server.js
@@ -17,6 +17,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected');
 
+  socket.emit('newMessage', {
+    from: 'Katrien',
+    text: 'Dag iedereen',
+    createdAt: 123123
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('client disconnected');
   })
