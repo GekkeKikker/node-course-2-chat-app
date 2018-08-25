fe5de9ed197ede4ca4314249b77e4f9c624af511
GekkeKikker <maarten.alles@pandora.be>
2018-08-25 23:02:08 +0200

    Add Users class and updateUserList event

--- server/server.js
+++ server/server.js
@@ -6,13 +6,15 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
-const {isRealString} = require('./utils/validation.js');
+const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT;
 
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var users = new Users();
 
 app.use(express.static(publicPath));
 
@@ -21,14 +23,13 @@ io.on('connection', (socket) => {
 
   socket.on('join', (params, callback) => {
     if (!isRealString(params.name) || !isRealString(params.room)) {
-      callback('Name and room are required');
+      return callback('Name and room are required');
     }
     socket.join(params.room);
-    //socket.leave('The Office fans');
+    users.removeUser(socket.id);
+    users.addUser(socket.id, params.name, params.room);
 
-    //io.emit -> io.to('The Office fans').newMessageHeight
-    //socket.broadcast.emit -> socket.broadcast.to('The Office fans').newMessageHeight
-    //socket.emit
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room));
 
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat app'));
     socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
@@ -46,7 +47,11 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
-    console.log('client disconnected');
+    var user = users.removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+      io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
+    }
   })
 });
 
